121352ac8f1b474c9002d44ca2269ba7e50b0466
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-05 12:59:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
-    <Navbar title="Anmol" />
+   <>
+    <Navbar title="Anmol"/>
+    <div className='container my-3'>
+      <TextForm heading="Enter the text to analyze" />
+    </div>
+   </>
+   
   );
 }
 
