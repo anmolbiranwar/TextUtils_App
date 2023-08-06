9853019157659322efe50cb6f29d86e59857d737
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-05 22:13:04 -0700

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,17 +5,36 @@ const TextForm = (props) => {
         let newText=text.toUpperCase();
         setText(newText);
     }
+    const handleLowClick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
+    const clear=()=>{
+        let newText="";
+        setText(newText);
+    }
     const handleOnChange=(event)=>{
         setText(event.target.value);
     }
     const [text, setText]= useState("Enter text here..");
   return (
    <>
-    <h1>{props.heading}</h1>
+    <div className="container">
+    <h2>{props.heading}</h2>
     <div className='mb-3'>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
+    <button className="btn btn-primary" onClick={clear}>Clear</button>
+
+    </div>
+    <div className="container">
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length} words {text.length} characters</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
    </>
   )
 }
