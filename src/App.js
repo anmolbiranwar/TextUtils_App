c8251e45b6d9db9c289c637e9fefa3007070ed29
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-10 11:50:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About';
+import TextForm from './components/TextForm';
+// import About from './components/About';
 
 function App() {
   return (
    <>
     <Navbar title="Anmol"/>
     <div className='container my-3'>
-      {/* <TextForm heading="Enter the text to analyze" /> */}
-      <About />
+      <TextForm heading="Enter the text to analyze" />
+      {/* <About /> */}
     </div>
    </>
    
