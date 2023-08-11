bad5683f0657bb6f983dd8632a22af0b0d416638
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-11 15:36:38 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
 
 function App() {
+  const [mode, setMode]=useState('light');//whether dark mode is enabled or not
+
   return (
    <>
-    <Navbar title="Anmol"/>
+    <Navbar title="TextUtils" mode={mode}/>
     <div className='container my-3'>
       <TextForm heading="Enter the text to analyze" />
       {/* <About /> */}
