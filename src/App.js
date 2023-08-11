96daf6bbf1e8c0676d59112fd65e3e1aca341898
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-11 16:09:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ function App() {
 const toggleMode=()=>{
   if(mode==='light'){
     setMode('dark');
+    document.body.style.backgroundColor='grey';
   }else{
     setMode('light');
+    document.body.style.backgroundColor='white';
   }
 }
   return (
    <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className='container my-3'>
-      <TextForm heading="Enter the text to analyze" />
+      <TextForm heading="Enter the text to analyze" mode={mode} />
       {/* <About /> */}
     </div>
    </>
