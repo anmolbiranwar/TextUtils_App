05a2991d7fe31bbe19f1e1ab0dfc798d1a3d3969
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-11 15:36:42 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import PropTypes from 'prop-types'
 const Navbar = (props) => {
   return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.title}</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -18,7 +18,7 @@ const Navbar = (props) => {
           </li>
         </ul>
       </div>
-    </div>
+    </div> 
   </nav>
   )
 }
