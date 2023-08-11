d616bbb048dfce653db0d1a2f2fcfb105395f138
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-11 15:54:46 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,6 +18,10 @@ const Navbar = (props) => {
           </li>
         </ul>
       </div>
+      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+        <input className="form-check-input" type="checkbox" role="switch" onClick={props.toggleMode} id="flexSwitchCheckDefault" />
+        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+</div>
     </div> 
   </nav>
   )
