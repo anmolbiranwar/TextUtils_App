99ca3014294021ee9585cdab1213869f5c0f2a9d
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-11 15:54:41 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import TextForm from './components/TextForm';
 
 function App() {
   const [mode, setMode]=useState('light');//whether dark mode is enabled or not
-
+const toggleMode=()=>{
+  if(mode==='light'){
+    setMode('dark');
+  }else{
+    setMode('light');
+  }
+}
   return (
    <>
-    <Navbar title="TextUtils" mode={mode}/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <div className='container my-3'>
       <TextForm heading="Enter the text to analyze" />
       {/* <About /> */}
