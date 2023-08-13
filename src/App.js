ccfa7527c6470ed22bdb287d3cecf3f4683dc5ab
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-12 18:38:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,11 @@ const toggleMode=()=>{
   if(mode==='light'){
     setMode('dark');
     document.body.style.backgroundColor='#042743';
+    showAlert("Dark mode has been enabled", "success");
   }else{
     setMode('light');
     document.body.style.backgroundColor='white';
+    showAlert("Light mode has been enabled", "success");
   }
 }
   return (
