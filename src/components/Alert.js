5f69c0359097200a6952db747946b87a3e612d01
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-12 18:34:18 -0700

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Alert(props) {
   return (
          <div className="alert alert-warning alert-dismissible fade show" role="alert">
-          <strong>{props.alert}</strong>
+          <strong>{props.alert.msg}</strong>
         </div>
   )
 }
