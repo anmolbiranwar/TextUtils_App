a4476807ed4e73154586c1d748512d10dd76aeab
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-12 18:19:42 -0700

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
-export default function Alert() {
+export default function Alert(props) {
   return (
-    <div>
-         <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+         <div className="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>{props.alert}</strong>
         </div>
-    </div>
   )
 }
