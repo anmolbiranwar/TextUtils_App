5fbdf232dd61f648ffa92b0a601b1ed2f127903a
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-16 13:34:51 -0700

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,10 +26,7 @@ const TextForm = (props) => {
     }
    
     const handleCopy=()=>{
-        var text=document.getElementById("myBox");
-        text.select();
         navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
         props.showAlert(" Copied to Clipboard!","success");
     }
     const [text, setText]= useState("");
