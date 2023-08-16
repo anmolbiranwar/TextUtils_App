79e1da714a6bd19d102fecc18ce26d4931cf72c2
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-08-16 13:29:00 -0700

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -50,7 +50,7 @@ const TextForm = (props) => {
     </div>
     <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your Text Summary</h2>
-        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
         <p>{0.008* text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Nothing to preview!"}</p>
@@ -59,4 +59,4 @@ const TextForm = (props) => {
   )
 }
 
-export default TextForm
+export default TextForm 
